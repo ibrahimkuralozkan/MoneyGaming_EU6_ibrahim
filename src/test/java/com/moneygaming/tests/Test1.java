22c16debf470fc2b3dae30dcf790f729dfc98da7
ibrahimkuralozkan <ibrahimkuralozkan@gmail.com>
2021-09-30 23:52:49 +0200

    i added my web driver factory utility class

--- src/test/java/com/moneygaming/tests/Test1.java
+++ src/test/java/com/moneygaming/tests/Test1.java
@@ -1,5 +1,7 @@
 package com.moneygaming.tests;
 
+import com.moneygaming.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -9,6 +11,7 @@ public class Test1 {
 
     @BeforeMethod
     public void setup(){
+    WebDriver driver = WebDriverFactory.getDriver("chrome");
 
 
     }
